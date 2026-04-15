79d4493cf87058038b010e229c73a438a83bb706
KomedawraOmusubi <tsubasay2346@gmail.com>
2026-04-15 23:39:16 +0900

    Update morning_job.py

--- morning_job.py
+++ morning_job.py
@@ -199,7 +199,7 @@ def main():
             driver.get(first_url)
             time.sleep(4)
 
-            for r in range(1, 13):
+            for r in range(1, 2):
                 try:
                     race_tabs = driver.find_elements(By.XPATH, f"//*[@data-raceno='{r}']")
                     if not race_tabs: break
