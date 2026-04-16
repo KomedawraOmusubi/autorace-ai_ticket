76efe1c28baaf15552d34f94bad4bddc5e516da2
KomedawraOmusubi <tsubasay2346@gmail.com>
2026-04-16 21:43:33 +0900

    Limit race processing to race number 5
    Change race range from 1-12 to 5-5 for specific processing.

--- morning_job.py
+++ morning_job.py
@@ -157,7 +157,7 @@ def main():
             driver.get(first_url)
             time.sleep(4)
 
-            for r in range(1, 13):
+            for r in range(5, 6):
                 try:
                     race_tabs = driver.find_elements(By.XPATH, f"//*[@data-raceno='{r}']")
                     if not race_tabs: break
