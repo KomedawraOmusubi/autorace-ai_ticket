94c3f5d55a599ffe02b064d35362fca37e820ecc
KomedawraOmusubi <tsubasay2346@gmail.com>
2026-04-19 20:29:29 +0900

    Update morning_job.py

--- morning_job.py
+++ morning_job.py
@@ -154,7 +154,7 @@ def main():
         except: active_places = []
 
         for place in active_places:
-            for r in range(10 ,11):
+            for r in range(12 ,13):
                 # ★ レースごとにURLを直接叩く
                 race_url = f"https://autorace.jp/race_info/Program/{place}/{today_str}_{r}/program"
                 driver.get(race_url)
